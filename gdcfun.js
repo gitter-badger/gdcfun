41e93011ca1476f8e22392e24b4a0e43d7f87e3e
Jonas Almeida <jalmeida@mathbiol.org>
2018-05-25 15:12:02 -0400

    ini cmd

--- gdcfun.js
+++ gdcfun.js
@@ -1,1 +1,8 @@
 console.log('gdcfun.js loaded')
+
+if(typeof(gdc)=='undefined'){
+    console.log("couldn't find gdc so loaded it")
+    let s = document.createElement('script')
+    s.src='https://mathbiol.github.io/gdc/gdc.js'
+    document.head.appendChild(s)
+}
