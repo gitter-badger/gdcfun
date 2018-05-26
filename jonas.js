bde2a7dbc471a360453f9ced78388ea0c413a0fb
Jonas Almeida <jalmeida@mathbiol.org>
2018-05-25 20:13:07 -0400

    making promises

--- jonas.js
+++ jonas.js
@@ -1,9 +1,36 @@
 console.log('jonas.js loaded')
 
 
-jonas=function(cmd){
+jonas= function(cmd){
+    this.url='https://api.gdc.cancer.gov'
     this.hello=()=>{
-        return 'hello from '+this+' at '+Date()
+        return 'hello from jonas at '+Date()
     }
-    debugger
+    this.get= async (cmd)=>{
+        cmd=cmd||'status' // default cmd is status
+        var r = (await fetch(this.url+'/'+cmd)).json()
+        return await r
+    }
+    // recording status when function is initiatized
+    var that=this
+    this.get().then(x=>{
+        that.status=x
+        console.log('initialization API status at '+Date()+':',x)
+    })
+    if(cmd){
+        return this.get(cmd)
+    }else{
+        return this
+    }
+    
 }
+
+// this one will automatically record status when the jonas.js is loaded
+
+// j = new jonas
+// then check
+// j.status
+
+// this one will returns promise to get status
+
+// j = new jonas('status')
