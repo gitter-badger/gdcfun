6bd0f467bb4f2fb2f41a43cb7835927799524bed
youdymoo <youdymoo@umich.edu>
2018-05-30 22:03:06 -0400

    init ray branch

--- ray.js
+++ ray.js
@@ -1,7 +1,26 @@
-console.log('ray.js')
+console.log('ray.js loaded')
 
 ray=function(){
-
+    this.url='https://api.gdc.cancer.gov'
+    this.hello=()=>{
+        return 'hello from ray at ' + Date()
+    }
+//     this.get= async (cmd)=>{
+//         cmd=cmd||'status' // default cmd is status
+//         var r = (await fetch(this.url+'/'+cmd)).json()
+//         return await r
+//     }
+//     // recording status when function is initiatized
+//     var that=this
+//     this.get().then(x=>{
+//         that.status=x
+//         console.log('initialization API status at '+Date()+':',x)
+//     })
+//     if(cmd){
+//         return this.get(cmd)
+//     }else{
+//         return this
+//     }
 }
 
 ray.times=(a, b)=>{
