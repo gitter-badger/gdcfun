93a299e81ac5c0923bf254740f14df0d86e8b549
m <xiaoham3@uci.edu>
2018-06-14 20:56:36 -0700

    commit before pull

--- xiaohan.js
+++ xiaohan.js
@@ -8,14 +8,15 @@ xiaohan = function(cmd) {
         cmd = cmd || 'status';
         try {
             const response = await fetch(this.url + '/' + cmd);
-            console.log(response.json());
+            console.log('fetch success');
+            return await response.json();
         }
         catch(err) {
             console.log('fetch failed', err);
         }
     }
 
-    
+
     if (cmd) {
         return this.cmd;
     } else {
