238cd3b853cb22295b18c6d35554afafa07bbe66
m <xiaoham3@uci.edu>
2018-06-28 11:08:40 -0700

    amd-require

--- xiaohan.js
+++ xiaohan.js
@@ -1,5 +1,9 @@
 console.log('xiaohan.js')
 
+
+
+
+
 xiaohan = function(cmd) {
     this.url = 'https://api.gdc.cancer.gov';
 
@@ -19,6 +23,21 @@ xiaohan = function(cmd) {
     }
 
     // endpoint: project
+
+    // endpoint: project, get all projects
+    this.getProjects = async function() {
+        endpoint = 'projects';
+        urlFetch = this.url + '/' + endpoint;
+        try {
+            var response = (await fetch(urlFetch)).json();
+            console.log('getProjects() success');
+            return await response;
+        } 
+        catch(err) {
+            console.log(err);
+        }
+    }
+
     // input: project id
     this.getProject = async function(id) {
         cmd = 'projects';
@@ -34,13 +53,26 @@ xiaohan = function(cmd) {
     }
 
     // endpoint: cases
-    // input: sample_id
+    this.getCases = async function() {
+        endpoint = 'cases';
+        urlFetch = this.url + '/' + endpoint;
+        try {
+            var response = (await fetch(urlFetch)).json();
+            console.log('getCases() success');
+            return await response;
+        } 
+        catch(err) {
+            console.log(err);
+        }
+    }
+
+    // get case by case id
     this.getCase = async function(id) {
         cmd = 'cases';
         urlFetch = this.url + '/' + cmd + '/' + id;
         try {
             var response = (await fetch(urlFetch)).json();
-            console.log('getCaset() success');
+            console.log('getCase() success');
             return await response;
         }
         catch(err) {
@@ -57,5 +89,8 @@ xiaohan = function(cmd) {
 }
 
 
-
-
+define(function(){
+    var exports = {};
+    exports.method = xiaohan;
+    return exports;
+})
